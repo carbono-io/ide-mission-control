b7b1c63c58631465e28fe06b4bc8d95f470f1e41
Aline Takechi <akt@cesar.org.br>
2015-08-31 17:23:54 -0300

    old communication with dcm to create project

--- app/lib/mission-control-bo.js
+++ app/lib/mission-control-bo.js
@@ -1,6 +1,7 @@
 'use strict';
 var CM = require('./code-machine-cli');
 var dcm = require('./ide-development-container-manager-cli');
+var url = require('url');
 
 /**
  * Pedir uma maquina pro dcm e nesse momento criar uma
@@ -13,17 +14,23 @@ exports.createDevContainer = function (project, cb) {
     dcm.create(project, function (err, res) {
         var cm;
         if (!err) {
-            var url = res.data.items[0].url;
+            // var url = res.data.items[0].url;
+            var path = url.format({
+                protocol: 'http',
+                hostname: res.host,
+                port: res.ports[1],
+            });
+
             var container = {
                 id: res.containerId,
-                markedURL: url + '/resources/marked',
-                cleanURL: url + '/resources/clean',
-                wsURL: url,
+                markedURL: path + '/resources/marked',
+                cleanURL: path + '/resources/clean',
+                wsURL: path,
             };
             cm = new CM(container);
             res = {
-                markedURL: url + '/resources/marked',
-                srcURL: url + '/resources',
+                markedURL: path + '/resources/marked',
+                srcURL: path + '/resources',
             };
         }
         cb(err, res, cm);
