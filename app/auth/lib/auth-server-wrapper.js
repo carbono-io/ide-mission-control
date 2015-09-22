7b5d31c5956b261cd16ede7273f9426201da64f6
Aline Takechi <akt@cesar.org.br>
2015-09-22 18:08:53 -0300

    [JRD-342] Token bearer authentication

---
+++ app/auth/lib/auth-server-wrapper.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var pjson   = require('../../../package.json');
+var CJM     = require('carbono-json-messages');
+var request = require('request');
+var uuid    = require('node-uuid');
+var q       = require('q');
+
+/**
+ * Try to parse the message, to extract user informations.
+ *
+ * @param {string} message - body attribute extracted from response object.
+ * @return {Object=} Object representing an User.
+ *
+ * @function
+ */
+function parseUser(message) {
+    message = JSON.parse(message);
+
+    var validStructure = message &&
+        message.hasOwnProperty('data') &&
+        message.data.hasOwnProperty('items') &&
+        message.data.items.length > 0 &&
+        message.data.items[0].hasOwnProperty('userInfo');
+
+    if (validStructure) {
+        return message.data.items[0].userInfo;
+    }
+
+    return null;
+}
+
+/**
+ * Builds the options used by request module.
+ *
+ * @param {string} token - Bearer Token
+ * @return {Object} Object representing options for request module.
+ *
+ * @function
+ */
+function buildRequestOptions(token) {
+    var cjm = new CJM({apiVersion: pjson.version});
+    cjm.setData({
+        id: uuid.v4(),
+        items: [
+            {
+                token: token,
+            },
+        ],
+    });
+
+    return {
+        uri: 'http://localhost:3000/carbono-auth/validate',
+        method: 'POST',
+        json: cjm.toObject(),
+    };
+}
+
+/**
+ * Find a user for the given token.
+ *
+ * @param {string} token - Bearer Token
+ * @return {Object} Object representing a promise.
+ *
+ * @function
+ */
+module.exports.findUser = function (token) {
+    if (token) {
+        var deffered = q.defer();
+
+        request(
+            buildRequestOptions(token),
+            function (err, res) {
+                if (err) {
+                    // Request error
+                    deffered.reject('[VALIDATE TOKEN] Request error: ' + err);
+
+                } else if (res.statusCode === 200) {
+                    // User retrieved
+                    var user = parseUser(res.body);
+
+                    if (user) {
+                        deffered.resolve(user);
+                    } else {
+                        deffered.reject(
+                            '[VALIDATE TOKEN] Can\'t read user informations.');
+                    }
+
+                } else if (res.statusCode === 404) {
+                    // User not found
+                    deffered.reject(null);
+
+                } else {
+                    // Unknown status code
+                    deffered.reject(
+                        '[VALIDATE TOKEN] Error: ' + res.statusCode);
+                }
+            });
+
+        return deffered.promise;
+    }
+
+    return null;
+};
---
+++ test/test-auth.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var authServer = require('../app/auth/lib/auth-server-wrapper');
+
+var chai = require('chai');
+var should = chai.should();
+
+describe('Authentication helper', function () {
+
+    it('finds an user with a valid token', function (done) {
+        var promise = authServer.findUser('token_valido');
+
+        promise.then(
+            function (user) {
+                should.exist(user);
+                user.should.be.an('object');
+
+                user.should.have.property('name');
+                should.equal(user.name, 'fulano');
+
+                user.should.have.property('code');
+                should.equal(user.code, '1234');
+
+                user.should.have.property('email');
+                should.equal(user.email, 'email@email.com');
+            },
+            function (err) {
+                should.fail(null, null, err);
+            }
+        )
+        .done(function () {
+            done();
+        });
+    });
+
+    it('can\'t find an user with an invalid token', function (done) {
+        var promise = authServer.findUser('token_invalido');
+
+        promise.then(
+            function () {
+                should.fail(null, null, 'this test should not return an user');
+            },
+            function (err) {
+                should.not.exist(err);
+            }
+        )
+        .done(function () {
+            done();
+        });
+    });
+
+    it('must identify request errors', function (done) {
+        var promise = authServer.findUser('status code desconhecido');
+
+        promise.then(
+            function () {
+                should.fail(null, null, 'this test should not return an user');
+            }, function (err) {
+                should.exist(err);
+                err.should.be.a('string');
+            }
+        )
+        .done(function () {
+            done();
+        });
+    });
+
+    it('must not create a promise when there\'s no token', function () {
+        var promise = authServer.findUser(null);
+        should.not.exist(promise);
+    });
+});
