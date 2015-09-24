b71a1dc25cbd15a4cf62e23d9e87f47aa3beda97
Aline Takechi <akt@cesar.org.br>
2015-09-24 18:13:13 -0300

    [JRD-342] test refactoring

--- test/test-auth.js
+++ test/test-auth.js
@@ -14,62 +14,51 @@ describe('Wrapper for carbono-auth', function () {
     it('finds an user with a valid token', function (done) {
         var promise = authServer.findUser('token_valido', this.urlMockAuth);
 
-        promise.then(
-            function (user) {
-                should.exist(user);
+        promise
+            .then(function (user) {
+                user.should.exist;
                 user.should.be.an('object');
 
                 user.should.have.property('displayName');
-                should.equal(user.displayName, 'fulano');
+                user.displayName.should.be.equal('fulano');
 
                 user.should.have.property('id');
-                should.equal(user.id, '1234');
+                user.id.should.be.equal('1234');
 
                 user.should.have.property('emails');
                 user.emails.should.have.lenght > 0;
                 user.emails[0].should.have.property('value');
-                should.equal(user.emails[0].value, 'email@email.com');
-            },
-            function (err) {
-                should.fail(null, null, err);
-            }
-        )
-        .done(function () {
-            done();
-        });
+                user.emails[0].value.should.be.equal('email@email.com');
+            })
+            .done(function () {
+                done();
+            });
     });
 
     it('can\'t find an user with an invalid token', function (done) {
         var promise = authServer.findUser('token_invalido', this.urlMockAuth);
 
-        promise.then(
-            function () {
-                should.fail(null, null, 'this test should not return an user');
-            },
-            function (err) {
+        promise
+            .catch(function (err) {
                 should.not.exist(err);
-            }
-        )
-        .done(function () {
-            done();
-        });
+            })
+            .done(function () {
+                done();
+            });
     });
 
     it('must identify request errors', function (done) {
         var promise = authServer
             .findUser('status code desconhecido', this.urlMockAuth);
 
-        promise.then(
-            function () {
-                should.fail(null, null, 'this test should not return an user');
-            }, function (err) {
+        promise
+            .catch(function (err) {
                 should.exist(err);
                 err.should.be.a('string');
-            }
-        )
-        .done(function () {
-            done();
-        });
+            })
+            .done(function () {
+                done();
+            });
     });
 
     it('must not create a promise when there\'s no token', function () {
