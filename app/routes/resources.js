944013f71c49e2c60396e090c25d2c58a576e698
Paul <deprazz@gmail.com>
2015-10-28 10:38:27 -0200

    Removed auth needed to access code-machine

--- app/routes/resources.js
+++ app/routes/resources.js
@@ -3,9 +3,9 @@ module.exports = function (app) {
     var resources = app.controllers.resources;
     var path = '/mc/cm/:projectId';
 
-    app.get(path + '/', app.authenticate, resources.root);
-    app.get(path + '/marked/[^**/]*', app.authenticate, resources.marked);
-    app.get(path + '/clean/[^**/]*', app.authenticate, resources.clean);
+    app.get(path + '/', resources.root);
+    app.get(path + '/marked/[^**/]*', resources.marked);
+    app.get(path + '/clean/[^**/]*', resources.clean);
 
     return this;
 };
